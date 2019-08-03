4ff2ecb8c936e00f80f09a2805dea6f637ff37e3
CisneFeijoo1998 <mdfeijoo@espol.edu.ec>
2019-08-03 15:42:49 -0500

    Commit inicial

---
+++ app/src/main/java/com/example/pst3_1t2019_aplicacionderiegoycontrolautomatizado/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.pst3_1t2019_aplicacionderiegoycontrolautomatizado;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
