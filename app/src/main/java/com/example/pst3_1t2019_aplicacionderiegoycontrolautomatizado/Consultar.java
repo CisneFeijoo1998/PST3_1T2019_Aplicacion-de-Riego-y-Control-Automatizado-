14b1a96c0563c2c614c652836f497db6198dc371
YovanaZR <ypzambra@espol.edu.ec>
2019-08-16 10:54:12 -0500

    correcciones

--- app/src/main/java/com/example/pst3_1t2019_aplicacionderiegoycontrolautomatizado/Consultar.java
+++ app/src/main/java/com/example/pst3_1t2019_aplicacionderiegoycontrolautomatizado/Consultar.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class Consultar extends AppCompatActivity {
     private Spinner spinner1;
@@ -16,13 +17,53 @@ public class Consultar extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consultar);
         spinner1= (Spinner)findViewById(R.id.spinner);
-<<<<<<< HEAD
-        String [] opciones= {"Enero","Febrero","Marzo","Abrl","Mayo","Junio","Julio","Agosto","Septiembre","Optubre","Noviembre","Diciembre"};
-=======
+
         String [] opciones= {"Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Optubre","Noviembre","Diciembre"};
->>>>>>> Yovana_Zambrano_Ruiz
         ArrayAdapter<String> adapter= new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, opciones);
         spinner1.setAdapter(adapter);
+    }
+    public void Consultar(View view){
+        String seleccion= spinner1.getSelectedItem().toString();
+        if (seleccion.equals("Enero")){
+
+            Toast.makeText(this, "Selección Exitosa", Toast.LENGTH_LONG).show();
+        } else if (seleccion.equals("Febrero ")){
+
+            Toast.makeText(this, "Selección Exitosa", Toast.LENGTH_LONG).show();
+        }else if (seleccion.equals("Marzo")){
+
+
+            Toast.makeText(this, "Selección Exitosa", Toast.LENGTH_LONG).show();
+        }else if (seleccion.equals("Abril")){
+
+            Toast.makeText(this, "Selección Exitosa", Toast.LENGTH_LONG).show();
+        }else if (seleccion.equals("Mayo")){
+
+            Toast.makeText(this, "Selección Exitosa", Toast.LENGTH_LONG).show();
+        }else if (seleccion.equals("Junio")){
+
+            Toast.makeText(this, "Selección Exitosa", Toast.LENGTH_LONG).show();
+        }else if (seleccion.equals("Julio")){
+
+            Toast.makeText(this, "Selección Exitosa", Toast.LENGTH_LONG).show();
+        }else if (seleccion.equals("Agosto")){
+
+            Toast.makeText(this, "Selección Exitosa", Toast.LENGTH_LONG).show();
+        }else if (seleccion.equals("Septiembre")){
+
+            Toast.makeText(this, "Selección Exitosa", Toast.LENGTH_LONG).show();
+        }else if (seleccion.equals("Octubre")){
+
+            Toast.makeText(this, "Selección Exitosa", Toast.LENGTH_LONG).show();
+        }else if (seleccion.equals("Noviembre")){
+
+            Toast.makeText(this, "Selección Exitosa", Toast.LENGTH_LONG).show();
+        }else if (seleccion.equals("Diciembre")){
+
+            Toast.makeText(this, "Selección Exitosa", Toast.LENGTH_LONG).show();
+        }
+
+
     }
 
     public void Ahorro(View view){
